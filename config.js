73b201d9d31be0856da6be843986a9eb89e652ee
Dwayne Charrington <dwaynecharrington@gmail.com>
2015-09-02 09:44:50 +1000

    Adding in needed plugin depoendencies

--- config.js
+++ config.js
@@ -15,14 +15,28 @@ System.config({
   },
 
   map: {
+    "aurelia-event-aggregator": "github:aurelia/event-aggregator@0.7.0",
+    "aurelia-http-client": "github:aurelia/http-client@0.10.3",
     "babel": "npm:babel-core@5.8.23",
     "babel-runtime": "npm:babel-runtime@5.8.20",
     "core-js": "npm:core-js@1.1.3",
+    "github:aurelia/event-aggregator@0.7.0": {
+      "aurelia-logging": "github:aurelia/logging@0.6.4"
+    },
+    "github:aurelia/http-client@0.10.3": {
+      "aurelia-path": "github:aurelia/path@0.8.1",
+      "core-js": "npm:core-js@0.9.18"
+    },
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
     "npm:babel-runtime@5.8.20": {
       "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:core-js@0.9.18": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
     "npm:core-js@1.1.3": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
