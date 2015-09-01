3453209a37f882882002cc730d2de765ddbeead8
Dwayne Charrington <dwaynecharrington@gmail.com>
2015-09-01 22:36:15 +1000

    Gulp and build tasks

---
+++ build/babel-options.js
@@ -0,0 +1,20 @@
+module.exports = {
+  filename: '',
+  filenameRelative: '',
+  modules: '',
+  sourceMap: true,
+  sourceMapName: '',
+  sourceRoot: '',
+  moduleRoot: '',
+  moduleIds: false,
+  experimental: false,
+  comments: false,
+  compact: false,
+  code:true,
+  stage:2,
+  loose: "all",
+  optional: [
+    "es7.decorators",
+    "es7.classProperties"
+  ]
+};
