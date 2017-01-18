aac9a48fb54de1532c2028a5ae78caf3fbc6e2d7
Dwayne Charrington <dwaynecharrington@gmail.com>
2017-01-18 21:21:58 +1000

    fix(tests): getting requirejs tests to work properly

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,6 @@ module.exports = function(config) {
     files: [
       'dist/test/test/main.js',
       { pattern: 'dist/test/**/*.js', included: false, watched: true },
-      //{ pattern: 'dist/test/**/*.html', included: false, watched: true },
       { pattern: 'node_modules/**/*.js', included: false, watched: false },
     ],
 
