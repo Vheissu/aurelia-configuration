42434e1232d9b37cbb06a1896f3fed2713d1053e
Dwayne Charrington <dwaynecharrington@gmail.com>
2017-02-18 23:24:56 +1000

    fix(plugin): typings were wrong or missing

--- src/aurelia-configuration.ts
+++ src/aurelia-configuration.ts
@@ -3,13 +3,13 @@ import deepExtend from './deep-extend';
 
 export class AureliaConfiguration {
 
-    private environment: string           = 'default';
+    private environment: string = 'default';
     private environments: string[] | null = null;
-    private directory: string             = 'config';
-    private config_file: string           = 'config.json';
-    private cascade_mode: boolean         = true;
+    private directory: string = 'config';
+    private config_file: string = 'config.json';
+    private cascade_mode: boolean = true;
 
-    private _config_object: {} | any       = {};
+    private _config_object: {} | any = {};
     private _config_merge_object: {} | any = {};
 
     /**
@@ -53,7 +53,7 @@ export class AureliaConfiguration {
      *
      * @param environments
      */
-    setEnvironments(environments = null) {
+    setEnvironments(environments: any = null) {
         if (environments !== null) {
             this.environments = environments;
 
@@ -72,7 +72,7 @@ export class AureliaConfiguration {
      *
      * @param bool
      */
-    setCascadeMode(bool = true) {
+    setCascadeMode(bool: boolean = true) {
         this.cascade_mode = bool;
     }
 
@@ -119,26 +119,24 @@ export class AureliaConfiguration {
     check() {
         let hostname = window.location.hostname;
 
-        if (!this.environments) {
-            return;
-        }
+        // Check we have environments we can loop
+        if (this.environments) {
+            // Loop over supplied environments
+            for (let env in this.environments) {
+                // Get environment hostnames
+                let hostnames = this.environments[env];
 
-        // Loop over supplied environments
-        for (let env in this.environments) {
-            // Get environment hostnames
-            let hostnames = this.environments[env];
+                // Make sure we have hostnames
+                if (hostnames) {
+                    // Loop the hostnames
+                    for (let host of hostnames) {
+                        if (hostname.search(host) !== -1) {
+                            this.setEnvironment(env);
 
-            if (!hostnames) {
-                return;
-            }
-
-            // Loop the hostnames
-            for (let host of hostnames) {
-                if (hostname == host) {
-                    this.setEnvironment(env);
-
-                    // We have successfully found an environment, stop searching
-                    return;
+                            // We have successfully found an environment, stop searching
+                            return;
+                        }
+                    }
                 }
             }
         }
@@ -175,7 +173,7 @@ export class AureliaConfiguration {
      * @param defaultValue
      * @returns {*}
      */
-    get(key: string, defaultValue = null) {
+    get(key: string, defaultValue: any = null) {
         // By default return the default value
         let returnVal = defaultValue;
 
@@ -201,8 +199,8 @@ export class AureliaConfiguration {
 
         if (key.indexOf('.') !== -1) {
             let splitKey = key.split('.');
-            let parent   = splitKey[0];
-            let child    = splitKey[1];
+            let parent = splitKey[0];
+            let child = splitKey[1];
 
             if (!this.environmentEnabled()) {
                 if (this.obj[parent]) {
@@ -234,8 +232,8 @@ export class AureliaConfiguration {
             this.obj[key] = val;
         } else {
             let splitKey = key.split('.');
-            let parent   = splitKey[0];
-            let child    = splitKey[1];
+            let parent = splitKey[0];
+            let child = splitKey[1];
 
             if (this.obj[parent] === undefined) {
                 this.obj[parent] = {};
@@ -334,7 +332,7 @@ export class AureliaConfiguration {
             }
             xhr.open('GET', pathClosure, true);
 
-            xhr.onreadystatechange = function () {
+            xhr.onreadystatechange = function() {
                 if (xhr.readyState == 4 && xhr.status == 200) {
                     let data = JSON.parse(this.responseText);
                     action(data);
@@ -348,7 +346,7 @@ export class AureliaConfiguration {
                 }
             };
 
-            xhr.onerror = function () {
+            xhr.onerror = function() {
                 reject(`Configuration file could not be found or loaded: ${pathClosure}`);
             };
 
